f0f9c82bfae0c2f7fd9a017157c121a645e4b538
NoahYe123 <jian.ye@mail.mcgill.ca>
2022-11-19 13:09:25 -0500

    made form page dynamic

--- frontend/src/pages/Form.js
+++ frontend/src/pages/Form.js
@@ -1,60 +1,81 @@
 import React from "react";
 import styled from "styled-components";
-
 import ReactDOM from "react-dom/client";
-import { StyledEngineProvider } from "@mui/material/styles";
 import { Select, MenuItem, Button } from "@mui/material";
 import { InputLabel } from "@mui/material";
 import FormControl from "@mui/material/FormControl";
+import { useState } from "react";
 
+ 
 const Form = () => {
-  return (
-    <Div>
-      <ParentForm>
-        Design
-        <FormControl
-          style={{
-            width: "47%",
-            marginTop: "5%",
-          }}
-        >
-          <InputLabel id="demo-simple-select-label"> Sub Category</InputLabel>
-          <Select>
-            <MenuItem value={10}>E-Commerce</MenuItem>
-            <MenuItem value={20}>Blog</MenuItem>
-            <MenuItem value={30}>Social Media</MenuItem>
-          </Select>
-        </FormControl>
-        <FormControl
-          style={{
-            width: "47%",
-            marginTop: "5%",
-          }}
-        >
-          <InputLabel id="demo-simple-select-label">Theme</InputLabel>
-          <Select>
-            <MenuItem value={10}>Futuristic</MenuItem>
-            <MenuItem value={20}>Modern</MenuItem>
-            <MenuItem value={30}>Old School</MenuItem>
-            <MenuItem value={30}>Web 3</MenuItem>
-          </Select>
-        </FormControl>
-        <FormControl
-          style={{
-            width: "47%",
-            marginTop: "5%",
-          }}
-        >
-          <InputLabel id="demo-simple-select-label">Color Theme</InputLabel>
-          <Select>
-            <MenuItem value={10}>Black</MenuItem>
-            <MenuItem value={20}>White</MenuItem>
-            <MenuItem value={30}>Orange</MenuItem>
-            <MenuItem value={30}>Brown</MenuItem>
-          </Select>
-        </FormControl>
-
-        <Button
+ const [subCategory, setSubCategory] = useState();
+ const [theme, setTheme] = useState();
+ const [color, setColor] = useState();
+ 
+ const handleSubmit = async (event) => {
+   if (subCategory != null) {
+     console.log(subCategory);
+   }
+ 
+   if (theme != null) {
+     console.log(theme);
+   }
+ 
+   if (color != null) {
+     console.log(color);
+   }
+ };
+ 
+ return (
+   <Div>
+     <ParentForm>
+       Design
+       <FormControl
+         style={{
+           width: "47%",
+           marginTop: "5%",
+         }}
+       >
+         <InputLabel id="demo-simple-select-label"> Sub Category</InputLabel>
+         <Select onChange={(e) => setSubCategory(e.target.value)}>
+           <MenuItem value={"E-Commerce"}>E-Commerce</MenuItem>
+           <MenuItem value={"Blog"}>Blog</MenuItem>
+           <MenuItem value={"Social Media"}>Social Media</MenuItem>
+         </Select>
+       </FormControl>
+       <FormControl
+         style={{
+           width: "47%",
+           marginTop: "5%",
+         }}
+       >
+         <InputLabel id="demo-simple-select-label">Theme</InputLabel>
+         <Select onChange={(e) => setTheme(e.target.value)}>
+           <MenuItem value={"Futureistic"}>Futuristic</MenuItem>
+           <MenuItem value={"Modern"}>Modern</MenuItem>
+           <MenuItem value={"Old School"}>Old School</MenuItem>
+           <MenuItem value={"Web 3"}>Web 3</MenuItem>
+         </Select>
+       </FormControl>
+       <FormControl
+         style={{
+           width: "47%",
+           marginTop: "5%",
+         }}
+       >
+         <InputLabel id="demo-simple-select-label">Color Theme</InputLabel>
+         <Select onChange={(e) => setColor(e.event.value)}>
+           <MenuItem value={"Black"}>Black</MenuItem>
+           <MenuItem value={"White"}>White</MenuItem>
+           <MenuItem value={"Orange"}>Orange</MenuItem>
+           <MenuItem value={"Brown"}>Brown</MenuItem>
+         </Select>
+       </FormControl>
+       <Button
+       type="submit"
+       onClick={() => {
+         handleSubmit();
+       }}
           variant="outlined"
           style={{
             marginTop: "5%",
@@ -70,38 +91,37 @@ const Form = () => {
         >
           Generate
         </Button>
-
-      </ParentForm>
-    </Div>
-  );
+     </ParentForm>
+   </Div>
+ );
 };
-
+ 
 const Div = styled.div`
-  padding: 20px;
-  margin: 10px;
+ padding: 20px;
+ margin: 10px;
 `;
 const ParentForm = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-
-  font-size: 40px;
-  font-family: 'Sono', sans-serif;;
-  margin: 0 20%;
-  padding: 15px;
+ display: flex;
+ flex-direction: column;
+ align-items: center;
+ justify-content: center;
+ font-size: 40px;
+ font-family: "Sono", sans-serif;
+ margin: 0 20%;
+ padding: 15px;
 `;
-
+ 
 const Category = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-  flex-direction: row;
-  font-weight: bold;
-  * {
-    margin-top: 20%;
-    left: 20%;
-  }
+ display: flex;
+ align-items: center;
+ justify-content: space-around;
+ flex-direction: row;
+ font-weight: bold;
+ * {
+   margin-top: 20%;
+   left: 20%;
+ }
 `;
-
+ 
 export default Form;
+
