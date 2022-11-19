c9361c0be042e15eb3f8a40866db65a837501e2b
williamye6 <williamxye2001@gmail.com>
2022-11-19 14:25:14 -0500

    header changes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,16 +1,26 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const Header = () => {
   return (
-    <Div>Header</Div>
-  )
-}
+    <Div>
+      Header
+      <Form>Form</Form>
+    </Div>
+  );
+};
 
 const Div = styled.div`
-  padding: 20px;
-  margin:10px;
+  padding: 1.75em;
+  width: 100%;
+  background-color: lightblue;
+  flex-direction: row;
+`;
 
-`
+const Form = styled.div`
+  flex-direction: row;
+  color: red;
+  font-weight: bold;
+`;
 
-export default Header
+export default Header;
