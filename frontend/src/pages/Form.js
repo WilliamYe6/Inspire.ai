abec199e231831e2d4c7616a4fc403bcc0c1659b
NoahYe123 <jian.ye@mail.mcgill.ca>
2022-11-19 02:56:43 -0500

    Used MUI for form page

--- frontend/src/pages/Form.js
+++ frontend/src/pages/Form.js
@@ -1,16 +1,58 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import ReactDOM from 'react-dom/client';
+import { StyledEngineProvider } from '@mui/material/styles';
+import { Select,MenuItem} from '@mui/material';
+import { InputLabel } from '@mui/material';
+import FormControl from '@mui/material/FormControl';
+
 const Form = () => {
   return (
     <Div>
       <ParentForm>
-          form
-      </ParentForm>
-      <ColorTheme>
-        COLOR THEME CATEGORY
-      </ColorTheme>
+        Design
 
+        <FormControl  style={{
+          width:  "47%",
+          marginTop: "5%"
+          
+        }} >
+        <InputLabel id="demo-simple-select-label">Category</InputLabel>
+        <Select     
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+
+
+
+
+      <FormControl  style={{
+          width:  "47%",
+          marginTop: "5%"
+          
+        }} >
+        <InputLabel id="demo-simple-select-label">Theme</InputLabel>
+        <Select     
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+
+
+      
+
+
+                 
+
+        
+      
+      </ParentForm>
     </Div>
   )
 }
@@ -22,24 +64,36 @@ const Div = styled.div`
 `
 const ParentForm = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
-  border: 1px solid black;
+  font-family:'Times New Roman', Times, serifç;
+  font-size:40px;
+
   margin: 0 20%;
   padding: 15px;
 `
 
-const ColorTheme = styled.div`
-  padding: 20px;
-  margin: 10px;
-  width: 100%;
-  left: 0px;
-  position: absolute;
-  text-align: center;
-  bottom: 100px;
-  color: red;
+const Category = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  flex-direction: row;
   font-weight: bold;
+  *{
+    margin-top: 20%;
+    left: 20%;
+
+  }
+
+
 `
+
+
+
+
+
+
 
 
 export default Form
