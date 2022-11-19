fa04a3ed3291a32291112faff791f3749f3b6f69
macsta31 <mackstathis@gmail.com>
2022-11-18 23:06:51 -0500

    create react app

--- backend/api/generate.js
+++ backend/api/generate.js
@@ -4,5 +4,17 @@ require('dotenv').config({path: '../config.env'})
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
-console.log(process.env.OPENAI_API_KEY)
+
 const openai = new OpenAIApi(configuration);
+
+const generate = async (prompt) => {
+    const response = await openai.createImage({
+        prompt: prompt,
+        n: 3,
+        size: "1024x1024",
+      });
+      images = response.data.data;
+    return [images[0].url, images[1].url, images[2].url]
+}
+
+module.exports = generate;
