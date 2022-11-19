7820e2dae4835b6fc107d11f61f011800391765e
williamye6 <williamxye2001@gmail.com>
2022-11-19 03:38:43 -0500

    updated form

--- frontend/src/pages/Form.js
+++ frontend/src/pages/Form.js
@@ -1,8 +1,9 @@
 import React from "react";
 import styled from "styled-components";
+
 import ReactDOM from "react-dom/client";
 import { StyledEngineProvider } from "@mui/material/styles";
-import { Select, MenuItem } from "@mui/material";
+import { Select, MenuItem, Button } from "@mui/material";
 import { InputLabel } from "@mui/material";
 import FormControl from "@mui/material/FormControl";
 
@@ -17,11 +18,11 @@ const Form = () => {
             marginTop: "5%",
           }}
         >
-          <InputLabel id="demo-simple-select-label">Category</InputLabel>
+          <InputLabel id="demo-simple-select-label"> Sub Category</InputLabel>
           <Select>
-            <MenuItem value={10}>Ten</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
+            <MenuItem value={10}>E-Commerce</MenuItem>
+            <MenuItem value={20}>Blog</MenuItem>
+            <MenuItem value={30}>Social Media</MenuItem>
           </Select>
         </FormControl>
         <FormControl
@@ -32,11 +33,42 @@ const Form = () => {
         >
           <InputLabel id="demo-simple-select-label">Theme</InputLabel>
           <Select>
-            <MenuItem value={10}>Ten</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
+            <MenuItem value={10}>Futuristic</MenuItem>
+            <MenuItem value={20}>Modern</MenuItem>
+            <MenuItem value={30}>Old School</MenuItem>
+            <MenuItem value={30}>Web 3</MenuItem>
           </Select>
         </FormControl>
+        <FormControl
+          style={{
+            width: "47%",
+            marginTop: "5%",
+          }}
+        >
+          <InputLabel id="demo-simple-select-label">Color Theme</InputLabel>
+          <Select>
+            <MenuItem value={10}>Black</MenuItem>
+            <MenuItem value={20}>White</MenuItem>
+            <MenuItem value={30}>Orange</MenuItem>
+            <MenuItem value={30}>Brown</MenuItem>
+          </Select>
+        </FormControl>
+
+        <Button
+          variant="outlined"
+          style={{
+            marginTop: "5%",
+            position: "relative",
+            borderRadius: "25px",
+            color: "black",
+            borderColor: "blue",
+            borderWidth: "2px",
+            width: "20%",
+          }}
+        >
+          Generate Image
+        </Button>
+
       </ParentForm>
     </Div>
   );
