c1b9d7e92ad869f002dbcd5a3d15aa5fe21b5669
williamye6 <williamxye2001@gmail.com>
2022-11-19 00:30:23 -0500

    added form

--- frontend/src/pages/Form.js
+++ frontend/src/pages/Form.js
@@ -1,9 +1,45 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Form = () => {
   return (
-    <div>this is the form page</div>
+    <Div>
+      <ParentForm>
+          form
+      </ParentForm>
+      <ColorTheme>
+        COLOR THEME CATEGORY
+      </ColorTheme>
+
+    </Div>
   )
 }
+
+const Div = styled.div`
+  padding: 20px;
+  margin: 10px;
+
+`
+const ParentForm = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid black;
+  margin: 0 20%;
+  padding: 15px;
+`
+
+const ColorTheme = styled.div`
+  padding: 20px;
+  margin: 10px;
+  width: 100%;
+  left: 0px;
+  position: absolute;
+  text-align: center;
+  bottom: 100px;
+  color: red;
+  font-weight: bold;
+`
+
 
 export default Form
