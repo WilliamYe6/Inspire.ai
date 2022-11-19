871bc59f1ada8e752bc8e2148b98c52130c1497a
macsta31 <mackstathis@gmail.com>
2022-11-18 23:24:37 -0500

    router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './pages/App';
+import Form from './pages/Form';
+import Dashboard from './pages/Dashboard';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+            <Route index element={<Dashboard />} />
+            <Route path='form' element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
 
 );
 
---
+++ frontend/src/pages/Form.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div>this is the form page</div>
+  )
+}
+
+export default Form
